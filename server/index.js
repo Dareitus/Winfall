cf36763071dcce06499f8533feca83b7c811bedf
Alex Kenney <dareitus@gmail.com>
2022-01-24 19:24:47 -0500

    server init

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
 // Server Code Goes Here
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/../dist'));
+
+//test route
+app.get('/test', (req, res) => {
+  res.send('Server Says Hello!');
+});
+
+//get user route
+app.get('/user', (req, res) => {
+  //check db for record at req.param.username
+  //if record exists, return record
+  //if no record, create record from base state, then return record
+});
+
+app.post('/user', (req, res) => {
+  //take req and update database with new player state
+});
+
+app.listen(port, () => {
+  console.log(`Server is now running at port: ${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 //Set Up React Dom Here.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./components/game.jsx";
+
+ReactDOM.render(<Game />, document.getElementById("app"));
